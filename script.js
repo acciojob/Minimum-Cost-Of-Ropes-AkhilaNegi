d6d66f07b9bfcd69416e04ef37ed769713a5cbaa
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-07-24 13:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
 function calculateMinCost() {
   //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
+ let lengths = document.getElementById('rope-lengths');
+ let values = lengths.value.split(', ').map(Number);
+ values.sort((a, b) => a - b);
+	let ans = 0;
+	while(values.length>2){
+		let val1 = values[0];
+		let val2 = values[1];
+		values.splice(0,2);
+		ans+=val1+val2;
+		values.push(val1+val2);
+		values.sort((a, b) => a - b);
+	}
+	ans+=values[0]+values[1];
+ let result = document.getElementById('result');
+	result.textContent = ans;
   
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
-  }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
+  
 }  
